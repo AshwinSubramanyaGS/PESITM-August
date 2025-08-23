f5f832ff14b7167062b38d4dee3fe49af4905443
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-08-23 11:01:26 +0530

    Day5

--- Day5/program1-module9.cpp
+++ Day5/program1-module9.cpp
@@ -6,15 +6,25 @@ using namespace std;
 class Student{
     string name;
     int age;
+    
 
     public: void setData(string,int);
     void displayData();
 };
 
+int main(){
+    int a;
+    string n;
+    cin>>n>>a;
+    Student s1;
+    s1.setData(n,a);
+    s1.displayData();
+
+}
 void Student::setData(string name,int age){
     this->name=name;
     this->age=age;
 }
 void Student::displayData(){
-    cout<<name<<endl<<age;
+    cout<<"Student Name: "<<name<<"\nStudent Age: "<<age;
 }
